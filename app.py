4db17e58f8deda5c3a334a81038625e62e87b500
kirinNeo <127916003+kirinNeo@users.noreply.github.com>
2023-06-19 14:56:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def communicate():
     messages.append(user_message)
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=messages
     )
 

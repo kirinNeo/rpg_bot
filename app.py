c85aa14d44004fa65d9818aa536a1e2b10683045
kirinNeo <127916003+kirinNeo@users.noreply.github.com>
2023-09-24 22:46:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -71,25 +71,6 @@ st.image("mr_runaway.png")
 st.write("※ChatGPT APIを使ったチャットボットです。")
 st.write("行動回数が0になる前にドラゴンから逃げ切ってください。")
 
-# 難易度の選択肢
-difficulty_options = ["簡単", "普通", "難しい"]
-
-# ユーザーに難易度を選ばせる
-difficulty = st.selectbox("難易度を選んでください：", difficulty_options)
-
-# 選択された難易度を保存
-st.session_state["difficulty"] = difficulty
-
-# 難易度に基づいてゲームの挙動を調整
-if st.session_state["difficulty"] == "簡単":
-    st.write("行動回数は７回です。初期装備は、「木の棒」です。")
-
-elif st.session_state["difficulty"] == "普通":
-    st.write("行動回数は５回です。初期装備は、「ナイフ」です。")
-    
-else:  # "難しい"
-    st.write("行動回数は３回です。初期装備は、「ライフル」です。")
-
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
